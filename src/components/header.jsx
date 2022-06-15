5f111512010bcd50f8ae5f6fc22257d43e91b4f5
G4 D3V <gabriel-dev0105@gmail.com>
2022-06-15 00:29:30 -0300

    ajuste semantica html e css

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,5 @@
 import { Box, HStack, Image } from '@chakra-ui/react'
+
 import { Button } from '../button'
 import logo from "../image/logo.png"
 
@@ -15,27 +16,35 @@ export const Header = ({ next, latest, past, upComing }) => {
   return (
     <HStack
       w='100%'
-      maxH='100px'
-      paddingX='350px'
-      justifyContent='space-around'
+      maxH='110px'
+      minH='50px'
       bg='#030508'
+      justifyContent='space-around'
     >
-      <Box h='100%' w='150px'>
+      <Box
+        display='flex'
+        flexDirection='row'
+        alignItems='center'
+        w='650px'
+        justifyContent='space-around'
+
+      >
         <Image
+          h='120px'
           paddingBottom='12px'
           objectFit='cover'
           src={logo}
           alt='logo-spaceX'
         />
-      </Box>
 
-      {
-        objButton.map((e) => (
-          <Button
-            action={e.action}
-            key={e.label}>{e.label}</Button>
-        ))
-      }
+        {
+          objButton.map((e) => (
+            <Button
+              action={e.action}
+              key={e.label}>{e.label}</Button>
+          ))
+        }
+      </Box>
 
     </HStack>
 
